3851ac203d7f855e2ba79a99f8d2485d9eca8f06
Jon Hodgins <19355502+JonHodgins@users.noreply.github.com>
2020-09-20 20:47:09 -0700

    Create CalculateStatistics overrides

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -48,5 +48,31 @@ namespace GradeBook.GradeBooks
                     return 'F';
             }
         }
+
+        public override void CalculateStatistics()
+        {
+            var numStudents = Students.Count;
+            if (numStudents < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            var numStudents = Students.Count;
+            if (numStudents < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
     }
 }
